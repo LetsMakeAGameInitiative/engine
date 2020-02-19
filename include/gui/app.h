5e0a14cf8e5fce7718ced8f9d0756fd95385b71c
satirev <tj.jaskolski@gmail.com>
2020-02-19 01:49:41 +0100

    Create skeleton library (#2)

---
+++ include/gui/app.h
@@ -0,0 +1,15 @@
+#ifndef LMAGIE_GUI_APP_H
+#define LMAGIE_GUI_APP_H
+
+namespace lmagie {
+namespace gui {
+	class app {
+	public:
+		app() = default;
+		~app() = default;
+		int run(int argc, char** argv);
+	};
+}
+}
+
+#endif
---
+++ source/gui/app.cpp
@@ -0,0 +1,9 @@
+#include "gui/app.h"
+#include <cstdio>
+
+namespace lmagie::gui {
+	int app::run(int argc, char** argv) {
+		std::puts("Hello from LMAGI engine");
+		return 0;
+	}
+}
