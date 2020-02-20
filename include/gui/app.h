af3cd9e76362bf1efa5314c8f838a50ec889b009
satirev <tj.jaskolski@gmail.com>
2020-02-20 01:38:39 +0100

    Updated CL after code review

--- include/gui/app.h
+++ include/gui/app.h
@@ -1,15 +1,13 @@
 #ifndef LMAGIE_GUI_APP_H
 #define LMAGIE_GUI_APP_H
 
-namespace lmagie {
-namespace gui {
-	class app {
-	public:
-		app() = default;
-		~app() = default;
-		int run(int argc, char** argv);
-	};
-}
+namespace lmagie::gui {
+  class app {
+  public:
+    app() = default;
+    ~app() = default;
+    int run(int argc, char** argv);
+  };
 }
 
 #endif
--- source/gui/app.cpp
+++ source/gui/app.cpp
@@ -2,8 +2,8 @@
 #include <cstdio>
 
 namespace lmagie::gui {
-	int app::run(int argc, char** argv) {
-		std::puts("Hello from LMAGI engine");
-		return 0;
-	}
+  int app::run(int argc, char** argv) {
+    std::puts("Hello from LMAGI engine");
+    return 0;
+  }
 }
